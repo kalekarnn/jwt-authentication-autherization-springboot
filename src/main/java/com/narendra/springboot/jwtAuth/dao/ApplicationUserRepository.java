0b0b297cee46c19c485b8f5e7f27fac11beead6f
narendra <kalekar.narendra@gmail.com>
2018-10-02 13:31:33 +0530

    Revert "Initial commit"
    This reverts commit 9e0629f93ced21449e94c406976b12a71d3ce4d5.

--- src/main/java/com/narendra/springboot/jwtAuth/dao/ApplicationUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.narendra.springboot.jwtAuth.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, String> {
-    ApplicationUser findUserByUsername(String username);
-}
