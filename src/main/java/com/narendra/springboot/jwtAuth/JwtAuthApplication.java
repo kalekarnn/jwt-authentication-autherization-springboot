58101ab3873790615cd8b61aac091d38e26149c7
narendra <kalekar.narendra@gmail.com>
2018-10-02 13:38:08 +0530

    Initial commit

--- src/main/java/com/narendra/springboot/jwtAuth/JwtAuthApplication.java
+++ src/main/java/com/narendra/springboot/jwtAuth/JwtAuthApplication.java
@@ -8,12 +8,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class JwtAuthApplication {
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwtAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthApplication.class, args);
+    }
 }
